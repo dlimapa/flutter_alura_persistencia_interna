60dbbc74fee8e522e6f18074d70b6ef1e3df36ff
Daniel de Lima <dlima@tre-pa.jus.br>
2020-04-29 22:40:18 -0300

    Primeira aula do curso Flutter Persistencia Interna

---
+++ android/app/src/main/kotlin/br/com/alura/flutter_persistencia_interna/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.alura.flutter_persistencia_interna
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
